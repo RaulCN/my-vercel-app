ce60002f56f2d68a6df28946f7f655ded6f58135
RaulCN <31525836+RaulCN@users.noreply.github.com>
2025-03-13 23:32:34 -0300

    Update send-demo.js

--- pages/api/send-demo.js
+++ pages/api/send-demo.js
@@ -1,9 +1,8 @@
-// pages/api/send-demo.js
 import twilio from 'twilio';
 
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-const twilioPhoneNumber = process.env.TWILIO_PHONE_NUMBER;
+const accountSid = process.env.TWILIO_ACCOUNT_SID; // Account SID
+const authToken = process.env.TWILIO_AUTH_TOKEN; // Auth Token
+const twilioPhoneNumber = process.env.TWILIO_PHONE_NUMBER; // Número do Twilio
 
 const client = twilio(accountSid, authToken);
 
